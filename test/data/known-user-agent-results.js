43d3ff96ee3ac319e31adbe8a9d035ec52754300
Keith Shaw <github@keithws.net>
2016-01-29 12:01:47 -0800

    Added additional test cases and device names

--- test/data/known-user-agent-results.js
+++ test/data/known-user-agent-results.js
@@ -1,5 +1,146 @@
 var expectedResults = [
   {
+    "device": "Samsung Galaxy S4",
+    "userAgent": "Mozilla/5.0 (Linux; Android 4.2.2; GT-I9505 Build/JDQ39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36",
+    "expectedReport": {
+      "browser": {
+        "name": "Chrome",
+        "version": "46.0.2490.76"
+      },
+      "os": {
+        "name": "Android",
+        "version": "4.2.2"
+      }
+    }
+  },
+  {
+    "device": "Samsung Galaxy S III",
+    "userAgent": "Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+    "expectedReport": {
+      "browser": {
+        "name": "Android Browser",
+        "version": "4.0"
+      },
+      "os": {
+        "name": "Android",
+        "version": "4.0"
+      }
+    }
+  },
+  {
+    "device": "Samsung Galaxy Note 3",
+    "userAgent": "Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+    "expectedReport": {
+      "browser": {
+        "name": "Android Browser",
+        "version": "4.0"
+      },
+      "os": {
+        "name": "Android",
+        "version": "4.3"
+      }
+    }
+  },
+  {
+    "device": "Samsung Galaxy Note II",
+    "userAgent": "Mozilla/5.0 (Linux; U; Android 4.1; en-us; GT-N7100 Build/JRO03C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+    "expectedReport": {
+      "browser": {
+        "name": "Android Browser",
+        "version": "4.0"
+      },
+      "os": {
+        "name": "Android",
+        "version": "4.1"
+      }
+    }
+  },
+  {
+    "device": "Nokia Lumia 520",
+    "userAgent": "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 520)",
+    "expectedReport": {
+      "browser": {
+        "name": "IE Mobile",
+        "version": "10.0"
+      },
+      "os": {
+        "name": "Windows Phone",
+        "version": "8.0"
+      }
+    }
+  },
+  {
+    "device": "LG Optimus L70",
+    "userAgent": "Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; LGMS323 Build/KOT49I.MS32310c) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/46.0.2490.76 Mobile Safari/537.36",
+    "expectedReport": {
+      "browser": {
+        "name": "Chrome",
+        "version": "46.0.2490.76"
+      },
+      "os": {
+        "name": "Android",
+        "version": "4.4.2"
+      }
+    }
+  },
+  {
+    "device": "Google Nexus 10",
+    "userAgent": "Mozilla/5.0 (Linux; Android 4.3; Nexus 10 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Safari/537.36",
+    "expectedReport": {
+      "browser": {
+        "name": "Chrome",
+        "version": "46.0.2490.76"
+      },
+      "os": {
+        "name": "Android",
+        "version": "4.3"
+      }
+    }
+  },
+  {
+    "device": "Google Nexus 4",
+    "userAgent": "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36",
+    "expectedReport": {
+      "browser": {
+        "name": "Chrome",
+        "version": "46.0.2490.76"
+      },
+      "os": {
+        "name": "Android",
+        "version": "4.4.2"
+      }
+    }
+  },
+  {
+    "device": "Google Nexus 6",
+    "userAgent": "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36",
+    "expectedReport": {
+      "browser": {
+        "name": "Chrome",
+        "version": "46.0.2490.76"
+      },
+      "os": {
+        "name": "Android",
+        "version": "5.1.1"
+      }
+    }
+  },
+  {
+    "device": "Google Nexus 5",
+    "userAgent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36",
+    "expectedReport": {
+      "browser": {
+        "name": "Chrome",
+        "version": "46.0.2490.76"
+      },
+      "os": {
+        "name": "Android",
+        "version": "6.0"
+      }
+    }
+  },
+  {
+    "device": "Firefox — Windows",
     "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:40.0) Gecko/20100101 Firefox/40.0",
     "expectedReport": {
       "browser": {
@@ -13,6 +154,7 @@ var expectedResults = [
     }
   },
   {
+    "device": "Firefox — Mac",
     "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:40.0) Gecko/20100101 Firefox/40.0",
     "expectedReport": {
       "browser": {
@@ -26,6 +168,7 @@ var expectedResults = [
     }
   },
   {
+    "device": "Chrome — Windows",
     "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
     "expectedReport": {
       "browser": {
@@ -39,6 +182,7 @@ var expectedResults = [
     }
   },
   {
+    "device": "Chrome — Mac",
     "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
     "expectedReport": {
       "browser": {
@@ -52,6 +196,21 @@ var expectedResults = [
     }
   },
   {
+    "device": "Internet Explorer 6",
+    "userAgent": "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+    "expectedReport": {
+      "browser": {
+        "name": "Internet Explorer",
+        "version": "6.0"
+      },
+      "os": {
+        "name": "Windows",
+        "version": "XP"
+      }
+    }
+  },
+  {
+    "device": "Internet Explorer 7",
     "userAgent": "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
     "expectedReport": {
       "browser": {
@@ -65,6 +224,7 @@ var expectedResults = [
     }
   },
   {
+    "device": "Internet Explorer 8",
     "userAgent": "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
     "expectedReport": {
       "browser": {
@@ -78,6 +238,21 @@ var expectedResults = [
     }
   },
   {
+    "device": "Internet Explorer 9 — Windows Vista",
+    "userAgent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)",
+    "expectedReport": {
+      "browser": {
+        "name": "Internet Explorer",
+        "version": "9.0"
+      },
+      "os": {
+        "name": "Windows",
+        "version": "Vista"
+      }
+    }
+  },
+  {
+    "device": "Internet Explorer 9 — Windows 7",
     "userAgent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
     "expectedReport": {
       "browser": {
@@ -91,6 +266,7 @@ var expectedResults = [
     }
   },
   {
+    "device": "Internet Explorer 10",
     "userAgent": "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)",
     "expectedReport": {
       "browser": {
@@ -104,6 +280,7 @@ var expectedResults = [
     }
   },
   {
+    "device": "Internet Explorer 11",
     "userAgent": "Mozilla/5.0 (Windows NT 6.3; Win64, x64; Trident/7.0; rv:11.0) like Gecko",
     "expectedReport": {
       "browser": {
@@ -117,6 +294,7 @@ var expectedResults = [
     }
   },
   {
+    "device": "Edge 12",
     "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136",
     "expectedReport": {
       "browser": {
@@ -130,6 +308,7 @@ var expectedResults = [
     }
   },
   {
+    "device": "Safari — iOS 9.1 — iPhone",
     "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
     "expectedReport": {
       "browser": {
@@ -143,6 +322,7 @@ var expectedResults = [
     }
   },
   {
+    "device": "Safari",
     "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/601.4.4 (KHTML, like Gecko) Version/9.0.3 Safari/601.4.4",
     "expectedReport": {
       "browser": {
--- test/user.agent.parsing.js
+++ test/user.agent.parsing.js
@@ -3,12 +3,9 @@
 describe("User Agent Matching", function () {
     expectedResults.forEach(function (item) {
         var expected = item.expectedReport;
-        var message = "should correctly identify {browser.name} {browser.version} on {os.name} {os.version}";
+        var message = "should correctly identify {device}";
 
-        message = message.replace("{browser.name}", expected.browser.name);
-        message = message.replace("{browser.version}", expected.browser.version);
-        message = message.replace("{os.name}", expected.os.name);
-        message = message.replace("{os.version}", expected.os.version);
+        message = message.replace("{device}", item.device);
 
         it(message, function () {
             var report = window.browserReportSync(item.userAgent);
